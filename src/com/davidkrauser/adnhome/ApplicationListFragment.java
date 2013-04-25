8b3aea4091d329bcd45dabf3593c701f8dc21d66
David Krauser <davidkrauser@me.com>
2013-04-24 22:00:12 -0400

    Cleaning up imports. Polishing application list UI.

--- src/com/davidkrauser/adnhome/ApplicationListFragment.java
+++ src/com/davidkrauser/adnhome/ApplicationListFragment.java
@@ -2,32 +2,21 @@ package com.davidkrauser.adnhome;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class ApplicationListFragment extends Fragment {
--- src/com/davidkrauser/adnhome/StreamFragment.java
+++ src/com/davidkrauser/adnhome/StreamFragment.java
@@ -2,14 +2,9 @@ package com.davidkrauser.adnhome;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
 
 public class StreamFragment extends Fragment {
  
