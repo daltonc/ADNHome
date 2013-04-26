9ad0cff97035a3559e9758025b08cdd5dc01fa29
David Krauser <davidkrauser@me.com>
2013-04-26 11:09:37 -0400

    Creating temporary webview for ADN content. This will be replaced by native code soon.

--- src/com/davidkrauser/adnhome/Launcher.java
+++ src/com/davidkrauser/adnhome/Launcher.java
@@ -6,59 +6,81 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.KeyEvent;
 
 public class Launcher extends FragmentActivity {
 
 	public static ADNModel sADNModel;
 	private LauncherPagerAdapter mLauncherPagerAdapter;
 	private ViewPager mViewPager;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.launcher);
 
-        sADNModel = new ADNModel();
-        
-        mLauncherPagerAdapter = new LauncherPagerAdapter(getSupportFragmentManager());
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.launcher);
 
-        mViewPager = (ViewPager) findViewById(R.id.launcher_pager);
-        mViewPager.setAdapter(mLauncherPagerAdapter);
-        mViewPager.setCurrentItem(1);        
-    }
-    
-    private class LauncherPagerAdapter extends FragmentPagerAdapter {
+		sADNModel = new ADNModel();
 
-        public LauncherPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+		mLauncherPagerAdapter = new LauncherPagerAdapter(
+				getSupportFragmentManager());
 
-        @Override
-        public Fragment getItem(int position) {
-        	Fragment fragment = null;
-        	
-        	switch(position)
-        	{
-        	case 0:
-        		fragment = new StreamFragment();
-        		break;
-        	case 1:
-        		fragment = new ApplicationListFragment();
-        		break;
-        	case 2:
-        		fragment = new MessagesFragment();
-        		break;
-        	default:
-        		break;
-        	}
-        	
-            return fragment;
-        }
+		mViewPager = (ViewPager) findViewById(R.id.launcher_pager);
+		mViewPager.setAdapter(mLauncherPagerAdapter);
+		mViewPager.setCurrentItem(1);
+	}
 
-        @Override
-        public int getCount() {
-            // Show 2 total pages.
-            return 3;
-        }
-    }
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (mViewPager.getCurrentItem() == 0
+					&& ((StreamFragment) ((LauncherPagerAdapter) mViewPager
+							.getAdapter()).getItem(0)).onKeyDown(keyCode)) {
+				return true;
+			} else {
+				mViewPager.setCurrentItem(1);
+				return true;
+			}
+		}
+
+		if (keyCode == KeyEvent.KEYCODE_HOME) {
+			mViewPager.setCurrentItem(1);
+			return true;
+		}
+
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private class LauncherPagerAdapter extends FragmentPagerAdapter {
+
+		public LauncherPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			Fragment fragment = null;
+
+			switch (position) {
+			case 0:
+				fragment = new StreamFragment();
+				break;
+			case 1:
+				fragment = new ApplicationListFragment();
+				break;
+			case 2:
+				fragment = new MessagesFragment();
+				break;
+			default:
+				break;
+			}
+
+			return fragment;
+		}
+
+		@Override
+		public int getCount() {
+			// Show 2 total pages.
+			return 2;
+		}
+	}
 }
--- src/com/davidkrauser/adnhome/StreamFragment.java
+++ src/com/davidkrauser/adnhome/StreamFragment.java
@@ -1,5 +1,8 @@
 package com.davidkrauser.adnhome;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
@@ -7,35 +10,64 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
 public class StreamFragment extends Fragment {
+	
+	public static WebView mWebView;
 
+	@SuppressLint("SetJavaScriptEnabled")
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.stream, container, false);
+		mWebView = (WebView) inflater.inflate(R.layout.stream, container, false);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.loadUrl("http://alpha.app.net");
+		
+		mWebView.setWebViewClient(new WebViewClient() {
+		    @Override
+		    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+		        if (Uri.parse(url).getHost().contains("app.net")) {
+		            return false;
+		        }
 
-		EditText composeStatusField = (EditText) view
-				.findViewById(R.id.compose_post_field);
+		        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+		        startActivity(intent);
+		        return true;
+		    }
+		});
 
-		composeStatusField
-				.setOnEditorActionListener(new OnEditorActionListener() {
-					public boolean onEditorAction(TextView v, int actionId,
-							KeyEvent event) {
-						boolean result = false;
+//		EditText composeStatusField = (EditText) view
+//				.findViewById(R.id.compose_post_field);
+//
+//		composeStatusField
+//				.setOnEditorActionListener(new OnEditorActionListener() {
+//					public boolean onEditorAction(TextView v, int actionId,
+//							KeyEvent event) {
+//						boolean result = false;
+//
+//						if (actionId == EditorInfo.IME_ACTION_SEND) {
+//							Launcher.sADNModel.dispatchPost(v.getText()
+//									.toString(), (EditText) v);
+//							result = true;
+//						}
+//
+//						return result;
+//					}
+//				});
 
-						if (actionId == EditorInfo.IME_ACTION_SEND) {
-							Launcher.sADNModel.dispatchPost(v.getText()
-									.toString(), (EditText) v);
-							result = true;
-						}
-
-						return result;
-					}
-				});
-
-		return view;
+		return mWebView;
 	}
+
+    public boolean onKeyDown(int keyCode) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        
+        return false;
+    }
 }
